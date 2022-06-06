02531be8dfec0f31665e09f62f5db2d44acec47a
bean <bkung188@gmail.com>
2022-06-06 15:19:33 -0700

    finished HTML and CSS

---
+++ src/script.js
@@ -0,0 +1,12 @@
+class Calculator {
+    constructor()
+}
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelectorAll('[data-equals]')
+const deleteButton = document.querySelectorAll('[data-delete]')
+const allClearButton = document.querySelectorAll('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
