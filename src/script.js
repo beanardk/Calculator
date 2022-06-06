b3d92dc86eea834e14644fc72944926fb4eeebe4
bean <bkung188@gmail.com>
2022-06-06 15:59:04 -0700

    added calculator class to JS file

--- src/script.js
+++ src/script.js
@@ -1,5 +1,34 @@
 class Calculator {
-    constructor()
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+    }
+
+    clear() {
+        this.currentOperand = ''
+        this.previousOperand =''
+        this.operation = undefined
+    }
+
+    delete() {
+
+    }
+
+    appendNumber(number) {
+        this.currentOperand = this.currentOperand.toString() + number.toString()
+    }
+
+    chooseOperation(operation) {
+
+    }
+
+    compute() {
+
+    }
+
+    updateDisplay() {
+        this.currentOperandTextElement.innerText = this.currentOperand
+    }
 }
 
 const numberButtons = document.querySelectorAll('[data-number]')
@@ -10,3 +39,11 @@ const allClearButton = document.querySelectorAll('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
 
+const calculator = new  Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
